8a76d977797046621c97796306f2283bd5f6cd7f
ryancharlessmith <ryan-smith@comcast.net>
2019-12-05 17:42:15 -0500

    css added link color change

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
     belongs_to :user
     accepts_nested_attributes_for :course
     validates :review_text, :rating, presence: true
+    validates :rating, inclusion: {in: 0..5 , message: "must be between zero and five"}
 end
